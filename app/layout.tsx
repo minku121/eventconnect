e7f149954b3c4eea3f9f7de4735e38073d90f39b
Minku Singh <minkupgh@gmail.com>
2025-03-28 19:14:32 +0530

    adding google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,27 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "TeamConnect - Create Team Collaboration",
   description: "Coonect with your team and collaborate on projects",
+  verification: {
+    google: "qL720GXw6dFNEOuVaNMFuRgmVM2BgQ0lKgMGKsqkmqo",
+  },
+  keywords: ["TeamConnect", "Event Collaboration", "Event Collaboration Platform", "event collaboration" , "Event collaboration platform" , "Join Event online",  "Event and certificate plateform" , "Team Collaboration", "Team Collaboration Platform", "Team Collaboration Tool", "Team Collaboration Software", "Team Collaboration App", "Team Collaboration System", "Team Collaboration Platform", "Team Collaboration Tool", "Team Collaboration Software", "Team Collaboration App", "Team Collaboration System"],
+  openGraph: {
+    title: "TeamConnect - Create Event Collaboration and Join event online",
+    description: "Coonect with your team and collaborate on projects",
+    type: "website",
+    locale: "en",
+    siteName: "TeamConnect",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TeamConnect - Create And Join event online",
+    description: "Coonect and create teams online with certifications",
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+  },
+  
 };
 
 export default function RootLayout({
